e064aada503417abe7d572922a006b183d4c319f
Danish Shakeel <danishshakeel54@gmail.com>
2021-12-28 00:21:32 +0530

    add bootstrapping processes

--- src/bootstrap.sh
+++ src/bootstrap.sh
@@ -1,8 +1,30 @@
 #! /bin/bash
 
-function bootstrap() {
-	echo 'Setting up permissions...'
-	chmod +x src/*.sh
-}
+# IMPORTS
+. "$PROJECT_ROOT/src/messages.sh"
 
-bootstrap
+###
+# function bootstrap()
+# bootstraps the project
+###
+function bootstrap() {
+	step 'Creating Logfile...'
+	filename="run-$(date +%s).log" # needs a better logic to create a unique filename; currently will create a new file everytime the script is run"
+	echo '' > "$PROJECT_ROOT/logs/$filename"
+	if [ $? -eq 0 ]; then
+		success 'Logfile created.'
+	else
+		error 'Error creating logfile; proceeding without one.'
+	fi
+
+	step 'Setting up permissions...'
+	chmod +x $PROJECT_ROOT/src/*.sh >> "$PROJECT_ROOT/logs/$filename" 2>&1
+
+	if [ $? -eq 0 ]; then
+		success 'Permissions set.'
+	else
+		error 'Error setting up permissions.'
+		warning 'Check logs for more information.'
+		exit 1
+	fi
+}
