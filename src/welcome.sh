e7e1f4502695f8a5be5518a6fee8fd7dd18d9809
Danish Shakeel <danishshakeel54@gmail.com>
2021-12-28 00:22:03 +0530

    add welcome message

---
+++ src/welcome.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+###
+# function welcome()
+# prints a welcome message
+###
+function welcome() {
+	prettify " 
+ ____ ____ ____ ____ ____ ____ ____ 
+||l |||e |||m |||p |||i |||f |||y ||
+||__|||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
+ " "34m"
+
+	prettify "A simple bash script to setup LEMP stack\n\n" $BOLD
+}
