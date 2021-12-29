54668cca5146f3dbf4ccc835360be16789e1e911
Danish Shakeel <danishshakeel54@gmail.com>
2021-12-29 23:16:47 +0530

    fix typo & add control logic to installer

--- src/installer.sh
+++ src/installer.sh
@@ -3,12 +3,15 @@
 ##
 # function install()
 # installs the required packages
+# returns 0 if the installations were successful; 1 otherwise
 ##
 function install() {
-	if ( ! $IS_NGINX_INSTALLED ); then
+	HAS_ERRORS=0
+	if  [ ! $IS_NGINX_INSTALLED ] ; then
 		step "Installing Nginx..."
 		sudo apt-get install nginx -y
 		if [ $? -ne 0 ]; then
+			HAS_ERRORS=1
 			error "Failed to install Nginx."
 			echo "[$(date)] - Failed to install Nginx." >> "$PROJECT_ROOT/logs/$filename" 2>&1
 		else
@@ -16,10 +19,11 @@ function install() {
 		fi
 	fi
 
-	if ( ! $IS_PHP_INSTALLED ); then 
+	if [ ! $IS_PHP_INSTALLED ] ; then 
 		step "Installing PHP..."
 		sudo apt-get install php-fpm php-cli php-mysql php-curl php-gd php-mcrypt php-imagick php-intl -y
 		if [ $? -ne 0 ]; then
+			HAS_ERRORS=1
 			error "Failed to install PHP."
 			echo "[$(date)] - Failed to install PHP." >> "$PROJECT_ROOT/logs/$filename" 2>&1
 		else
@@ -27,14 +31,17 @@ function install() {
 		fi
 	fi
 
-	if ( ! $IS_MYSQL_INSTALLED ); then
+	if [ ! $IS_MYSQL_INSTALLED ] ; then
 		step "Installing MySQL..."
 		sudo apt-get install mysql-server -y
 		if [ $? -ne 0 ]; then
+			HAS_ERRORS=1
 			error "Failed to install MySQL."
 			echo "[$(date)] - Failed to install MySQL." >> "$PROJECT_ROOT/logs/$filename" 2>&1
 		else
 			success "MySQL installed successfully."
 		fi
 	fi
+
+	return HAS_ERRORS
 }
