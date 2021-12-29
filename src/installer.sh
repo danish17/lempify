b56a40dcf70bb62a8efb8a64b1269c28cd253c86
Danish Shakeel <danishshakeel54@gmail.com>
2021-12-29 23:06:51 +0530

    add installer scripts

--- src/installer.sh
+++ src/installer.sh
@@ -1,5 +1,40 @@
 #! /bin/bash
 
+##
+# function install()
+# installs the required packages
+##
 function install() {
-	echo "Installing"
+	if ( ! $IS_NGINX_INSTALLED ); then
+		step "Installing Nginx..."
+		sudo apt-get install nginx -y
+		if [ $? -ne 0 ]; then
+			error "Failed to install Nginx."
+			echo "[$(date)] - Failed to install Nginx." >> "$PROJECT_ROOT/logs/$filename" 2>&1
+		else
+			success "Nginx installed successfully."
+		fi
+	fi
+
+	if ( ! $IS_PHP_INSTALLED ); then 
+		step "Installing PHP..."
+		sudo apt-get install php-fpm php-cli php-mysql php-curl php-gd php-mcrypt php-imagick php-intl -y
+		if [ $? -ne 0 ]; then
+			error "Failed to install PHP."
+			echo "[$(date)] - Failed to install PHP." >> "$PROJECT_ROOT/logs/$filename" 2>&1
+		else
+			success "PHP installed successfully."
+		fi
+	fi
+
+	if ( ! $IS_MYSQL_INSTALLED ); then
+		step "Installing MySQL..."
+		sudo apt-get install mysql-server -y
+		if [ $? -ne 0 ]; then
+			error "Failed to install MySQL."
+			echo "[$(date)] - Failed to install MySQL." >> "$PROJECT_ROOT/logs/$filename" 2>&1
+		else
+			success "MySQL installed successfully."
+		fi
+	fi
 }
